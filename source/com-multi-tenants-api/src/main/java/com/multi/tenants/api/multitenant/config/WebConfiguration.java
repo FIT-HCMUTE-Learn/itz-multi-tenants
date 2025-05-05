56b4459249bfdd3803e0e288ee027a8dc473786e
PhucLe0809 <lhphuc.7.01j@gmail.com>
2025-05-05 16:34:24 +0700

    Fix to check migrate when create new connection pool

--- source/com-multi-tenants-api/src/main/java/com/multi/tenants/api/multitenant/config/WebConfiguration.java
+++ source/com-multi-tenants-api/src/main/java/com/multi/tenants/api/multitenant/config/WebConfiguration.java
@@ -1,7 +1,6 @@
 package com.multi.tenants.api.multitenant.config;
 
 import com.multi.tenants.api.multitenant.interceptor.TenantInterceptor;
-import com.multi.tenants.api.multitenant.interceptor.TenantMigrationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,22 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final TenantInterceptor tenantInterceptor;
-    private final TenantMigrationInterceptor tenantMigrationInterceptor;
 
     @Autowired
-    public WebConfiguration(TenantInterceptor tenantInterceptor, TenantMigrationInterceptor tenantMigrationInterceptor) {
+    public WebConfiguration(TenantInterceptor tenantInterceptor) {
         this.tenantInterceptor = tenantInterceptor;
-        this.tenantMigrationInterceptor = tenantMigrationInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // First set the tenant context
+        // Only set the tenant context
         registry.addWebRequestInterceptor(tenantInterceptor);
-
-        // Then check and perform migration if needed
-        registry.addInterceptor(tenantMigrationInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/static/**", "/webjars/**", "/error/**");
     }
 }
